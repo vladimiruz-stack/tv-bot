5bd25cc49cc0fac4774cfe6b3cc02b823e2a3b41
vladimiruz-stack <vladimiruz@gmail.com>
2026-04-25 00:38:19 +0300

    Update tv_alert.py

--- tv_alert.py
+++ tv_alert.py
@@ -1,83 +1,15 @@
-import os
 import requests
-from bs4 import BeautifulSoup
 
 BOT_TOKEN = "8685078633:AAGD5YwBE1u5Ii1kPwB9rZ6Jp4tNDdv57NU"
 CHAT_ID = "38908105"
 
-PROGRAMS = [
-    "Клара Румянова. Звезда за кадром",
-    "НТВ. Дни творения",
-    "Перо и шпага Валентина Пикуля",
-    "Эдуард Хиль - Сто хитов короля эстрады",
-    "Гоголь и ляхи",
-    "Спето в СССР. Госпожа удача",
-    "Сталин и писатели",
-    "Уроки пения",
-    "Владимир Володин. Опереточный герой",
-    "Николай Гумилев. Завещание",
-    "Никулин и Шуйдин",
-    "Алексей Попов. Трагедия в трех актах с прологом и эпилогом",
-    "В поисках Франции с Вадимом Глускером",
-    "Тайны лазурного берега",
-    "Последняя капля",
-    "Маленькое черное платье",
-    "Снять по-французски",
-    "Хранители наследства",
-    "Другая жизнь Натальи Шмельковой",
-    "Исторические путешествия с Иваном Толстым",
-    "Стрит-Арт. Философия прямого действия",
-    "Мировые леди",
-    "Проявления Павла Каплевича",
-    "Последний тусовщик Оттепели",
-    "Вначале было дело или История русской промышленности",
-]
+url = f"https://api.telegram.org/bot{8685078633:AAGD5YwBE1u5Ii1kPwB9rZ6Jp4tNDdv57NU}/sendMessage"
 
-CHANNELS = [
-    "Культура",
-    "Первый канал",
-    "Россия 1",
-    "Россия",
-    "НТВ",
-    "ТВ Центр",
-    "Пятый канал",
-    "РЕН ТВ",
-    "Звезда",
-    "Мир",
-    "Победа",
-    "Дом кино",
-    "Время",
-    "ОТР",
-    "Россия 24",
-    "Карусель",
-]
-]
+r = requests.post(
+    url,
+    json={"chat_id": CHAT_ID, "text": "ТЕСТ 🚀"},
+    timeout=20
+)
 
-SEARCH_URL = "https://tv.yandex.ru/search?text="
-
-def send_telegram(text):
-    r = requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={"chat_id": CHAT_ID, "text": text},
-        timeout=20
-    )
-    print(r.status_code)
-    print(r.text)
-
-def check_program(title):
-    url = SEARCH_URL + requests.utils.quote(title)
-    html = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=20).text
-    text = BeautifulSoup(html, "html.parser").get_text(" ", strip=True)
-
-    found_channel = next((ch for ch in CHANNELS if ch in text), None)
-
-    if found_channel:
-        return f"Нашёл возможный эфир:\n«{title}»\nКанал: {found_channel}\n{url}"
-
-    return None
-
-def main():
-    send_telegram("ТЕСТ 🚀")
-
-if __name__ == "__main__":
-    main()
+print("STATUS:", r.status_code)
+print("RESPONSE:", r.text)
